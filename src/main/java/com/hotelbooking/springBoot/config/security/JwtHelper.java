f7a0c248bd78d0992bc1180d5430b1cd57e4b68e
bipinstha07 <shresthab2@warhawks.ulm.edu>
2025-08-01 02:04:28 -0500

    TokenValidation and Expiration

--- src/main/java/com/hotelbooking/springBoot/config/security/JwtHelper.java
+++ src/main/java/com/hotelbooking/springBoot/config/security/JwtHelper.java
@@ -1,11 +1,13 @@
 package com.hotelbooking.springBoot.config.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
@@ -32,7 +34,28 @@ public class JwtHelper {
                 .compact();
     }
 
+    public String getUserNameFromToken(String token){
+        return getClaims(token).getSubject();
+    }
 
+    public boolean isTokenValid(String token,UserDetails userDetails){
+        String username = getUserNameFromToken(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token){
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+
+//    This method parses a JWT and extracts claims (data inside the token):
+    private Claims getClaims(String token){
+        return Jwts.parser()
+                .verifyWith((SecretKey) key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 
 
 
